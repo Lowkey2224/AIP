856d3cf92220dccabffaef2a998b32f024cea0a0
Lowkey2224 <marcus.jenz@gmail.com>
2013-12-03 11:54:11 +0100

    added first tests for Kunde

---
+++ src/mps/kunden/tests/KundeTest.java
@@ -0,0 +1,42 @@
+package mps.kunden.tests;
+
+
+import junit.framework.Assert;
+import mps.kunden.dtos.KundeDTO;
+import mps.kunden.entities.Kunde;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * User: Loki
+ * Date: 03.12.13
+ * Time: 11:41
+ */
+public class KundeTest {
+
+    Kunde kunde1;
+    KundeDTO kundeDTO1, kundeDTO2;
+
+    @Before
+    public void setup()
+    {
+        kunde1 = new Kunde();
+        kunde1.setAddress("Adresse");
+        kunde1.setName("name");
+        kunde1.setNr(23);
+        kundeDTO1 = new KundeDTO("name", "Adresse", 23);
+        kundeDTO2 = new KundeDTO("name2", "Adresse2", 42);
+    }
+
+    @Test
+    public void testToDTO() throws Exception {
+        KundeDTO test1 = kunde1.toDTO();
+        assert(!test1.equals(kundeDTO2));
+    }
+
+    @Test
+    public void testFromDTO() throws Exception {
+        Kunde k = Kunde.fromDTO(kundeDTO1);
+        assert (k.equals(kunde1));
+    }
+}
